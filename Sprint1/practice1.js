c04ffa2e571b5a7fd51de40118d49cae9fcb68ab
Nasee-t <lunagood0123@gmail.com>
2025-02-19 15:09:45 +0530

    Add files via upload

---
+++ Sprint1/practice1.js
@@ -0,0 +1,64 @@
+//1
+console.log("Hello World!");
+
+//2
+console.log("Naseeha");
+
+// 3
+console.log("Naseeha \n20");
+
+// 4
+let string = "hello";
+let num=5;
+let bool=true;
+let numstr="7";
+console.log(num + numstr);
+console.log(bool + num);
+
+// 5
+console.log("Naseeha" + " 20");
+
+// 6
+let name = "Naseeha" ;
+let age=20;
+console.log(name + age + "\n");
+
+// 7
+let a = 10;
+let b = 5;
+
+console.log("Addition: " + (a + b));
+console.log("Subtraction: " + (a - b));
+console.log("Multiplication: " + (a * b));
+console.log("Division: " + (a / b));
+console.log("Modulus: " + (a % b));
+console.log("Exponentiation: " + (a ** b));
+
+// 8
+let arr= [ 1, 2, 3];
+console.log(arr);
+console.log(arr[0]);
+
+// 9
+for (let i = 1; i <= 4; i++) {
+        console.log("*".repeat(i));
+}
+
+// 10
+let name1 = "Naseeha";
+let age1=20;
+let isStudent=true;
+
+//11
+name1="Nasee";
+age1++;
+isStudent=false;
+
+// 12
+let name2 = "Naseeha";
+let age2=20;
+let dob = "2004-09-20";
+let height=157.99;
+let weight=50;
+let degree="CSE";
+let gender="Female";
